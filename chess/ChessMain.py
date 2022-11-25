0247a4a47389e9f066040b19bab127b484232710
cs572go <tushartushar572@gmail.com>
2022-11-25 08:25:46 +0530

    update

--- chess/ChessMain.py
+++ chess/ChessMain.py
@@ -51,7 +51,7 @@ def main():
     move_finder_process = None
     move_log_font = p.font.SysFont("Arial", 14, False, False)
     player_one = True  # if a human is playing white, then this will be True, else False
-    player_two = False  # if a hyman is playing white, then this will be True, else False
+    player_two = False  # if a human is playing black, then this will be True, else False
 
     while running:
         human_turn = (game_state.white_to_move and player_one) or (not game_state.white_to_move and player_two)
